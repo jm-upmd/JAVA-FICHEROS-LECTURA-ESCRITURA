51fc1e985c2fd9bf0ee5e3ef21617db6992d3ff4
Jose Manuel <jmdominguez@aytoalcobendas.org>
2021-02-10 21:09:52 +0100

    Arriglado escritura binaria

--- src/lecturaEscrituraFicheros/CopiaFicherosBinario.java
+++ src/lecturaEscrituraFicheros/CopiaFicherosBinario.java
@@ -4,17 +4,23 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CopiaFicherosBinario {
 
 	public static void main(String[] args) {
-		final String F_ORIGEN = "F:\\\\Curso Java\\\\Proyectos Eclipse\\\\ficheroOut.txt";
-		final String F_DESTINO = "F:\\Curso Java\\Proyectos Eclipse\\ficheroOut.copia.txt";
+		final String F_ORIGEN = "H:\\Plano Aula.xlsx";
+		final String F_DESTINO = "H:\\Copia del Fiechero.xlsx";
 		copia (F_ORIGEN,F_DESTINO );
 	}
 	
-	public static void copia (String ficheroOriginal, String ficheroCopia)
+	public static void copia (String ficheroOriginal, String ficheroCopia) 
 	{
+		FileInputStream fileInput;
+		BufferedInputStream bufferedInput = null;
+		FileOutputStream fileOutput;
+		BufferedOutputStream bufferedOutput = null; 
+		
 		try
 		{
             // Se abre el fichero original para lectura.
@@ -23,32 +29,47 @@ public class CopiaFicherosBinario {
 			// También puede usarse para leer ficheros de texto byte a byte, pero para estos es
 			// mejor usar la clase  FileReader
 			
-			FileInputStream fileInput = new FileInputStream(ficheroOriginal);
-			BufferedInputStream bufferedInput = new BufferedInputStream(fileInput);
+			fileInput = new FileInputStream(ficheroOriginal);
+			bufferedInput = new BufferedInputStream(fileInput);
 			
 			// Se abre el fichero donde se hará la copia
-			FileOutputStream fileOutput = new FileOutputStream (ficheroCopia);
-			BufferedOutputStream bufferedOutput = new BufferedOutputStream(fileOutput);
+			 fileOutput = new FileOutputStream (ficheroCopia);
+			 bufferedOutput = new BufferedOutputStream(fileOutput);
 			
-			// Bucle para leer de un fichero y escribir en el otro.
+			
+			 System.out.println("Copiando fichero...");
+			 // Bucle para leer de un fichero y escribir en el otro.
 			// Utilizamos un array de bytes donde se irán depositando los datos del fichero origen
 			// por bufferredInput.read() y de donde serán leídos por burreredOutput.write
+			
 			byte [] array = new byte[1000];
+			
 			int leidos = bufferedInput.read(array); // leidos es el numro de bytes leidos y depositados en array
 			while (leidos > 0)
 			{
 				bufferedOutput.write(array,0,leidos); // Lee desde posición 0 tantos elementos como indica leidos
 				leidos=bufferedInput.read(array);
 			}
+			
+			System.out.println("Copia realizada");
 
-			// Cierre de los ficheros
-			bufferedInput.close();
-			bufferedOutput.close();
+		
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
-		}
+		} finally {
+			
+		}	// Cierre de los ficheros OJO
+
+			try {
+				bufferedInput.close();
+				bufferedOutput.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 	}
 
 }
